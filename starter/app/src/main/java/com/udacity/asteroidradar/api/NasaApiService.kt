ba0b8c740a2fd6dc89d66133e4da1dfd0c828306
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-05-17 13:01:19 +0100

    Testing if results are fetched correctly in MainViewModel.kt

--- starter/app/src/main/java/com/udacity/asteroidradar/api/NasaApiService.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/api/NasaApiService.kt
@@ -1,18 +1,27 @@
 package com.udacity.asteroidradar.api
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.PictureOfDay
+import org.json.JSONObject
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 private const val API_KEY = "Acb3wXMWBcliyVZeNUFuY5j4YWXZ2ERNm6an7nYM"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(Constants.BASE_URL)
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .build()
 
 interface NasaApiService {
@@ -21,12 +30,12 @@ interface NasaApiService {
         @Query(Constants.START_DATE) startDate:String = getCurrentDate(),
         @Query(Constants.END_DATE) endDate:String = getSevenDaysLaterDate(),
         @Query(Constants.API_KEY) apiKey:String = API_KEY
-    ): Response<String>
+    ): String
 
     @GET("/planetary/apod")
     suspend fun getTodayImage(
         @Query(Constants.API_KEY) apiKey:String = API_KEY
-    ): Response<PictureOfDay>
+    ): PictureOfDay
 }
 
 object NasaApi {
--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,8 +1,28 @@
 package com.udacity.asteroidradar.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.api.NasaApi
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 class MainViewModel : ViewModel() {
 
+init {
+    getNasaAsteroids()
+}
 
+    private fun getNasaAsteroids() {
+        viewModelScope.launch {
+            try {
+                val response = NasaApi.retrofitService.getAsteroids()
+                val result = parseAsteroidsJsonResult(JSONObject(response))
+                Log.d("GGG", "asteroids: ${result.size}")
+            } catch (e: Exception){
+                Log.d("ggg", "error: $e")
+            }
+        }
+    }
 }
