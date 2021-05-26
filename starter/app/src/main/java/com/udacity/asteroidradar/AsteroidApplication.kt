cdfd7e299cc0d4707d35dbf3e364de98bebfc89b
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-05-26 19:42:10 +0100

    Added constraints to WorkManager

--- starter/app/src/main/java/com/udacity/asteroidradar/AsteroidApplication.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/AsteroidApplication.kt
@@ -1,9 +1,8 @@
 package com.udacity.asteroidradar
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import com.udacity.asteroidradar.work.RefreshAsteroidsWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -26,9 +25,21 @@ class AsteroidApplication : Application() {
     }
 
     private fun setUpRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
         val repeatingRequest = PeriodicWorkRequestBuilder<RefreshAsteroidsWorker>(
-            1,TimeUnit.DAYS
-        ).build()
+            1,TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
 
         WorkManager.getInstance().enqueueUniquePeriodicWork(
             RefreshAsteroidsWorker.WORK_NAME,
